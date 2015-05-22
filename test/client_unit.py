ef4fbb3997452771945192eef6ae70098b99ddc1
Benjamin Jones <ben@benjaminjones.me>
2015-05-21 20:38:34 -0400

    Added register client unit test

--- test/client_unit.py
+++ test/client_unit.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 from enum import Enum
+import struct
+import socket
+import sys
 
 class MessageType(Enum):
     REGISTER_TETRAD = 0 
@@ -10,24 +13,33 @@ class MessageType(Enum):
     DISCONNECT_CLIENT = 4
     KICK_CLIENT = 5
     CREATE_ROOM = 6
-    NUM_MESSAGES = 7
+    USER_ACTION = 7
+    NUM_MESSAGES = 8 
 
 
 class Message:
 	type = None
 	length = 0
 
+
 class RegisterTetrad(Message):
 	type = MessageType.REGISTER_TETRAD
 
+	def pack(self):
+		return struct.pack("!ch", self.type, 0)
+
 class RegisterClient(Message):
 	type = MessageType.REGISTER_CLIENT
-	length = 1
+	length = 0 
 	name = "" 
 
 	def setName(self, name):
 		self.name = name
 		length = length + len(name)
+	def pack(self):
+		return struct.pack("!chs", self.type, self.length, self.name)
+
+		
 
 class UpdateTetrad(Message):
 	type = MessageType.UPDATE_TETRAD
@@ -41,6 +53,8 @@ class UpdateTetrad(Message):
 		self.y0 = y
 	def setRotation(self, rot):
 		self.rot = rot
+	def pack(self):
+		return struct.pack("!chiiiii", self.x, self.y, self.x0, self.y0, self.rot)
 
 class UpdateClientState(Message):
 	type = MessageType.UPDATE_CLIENT_STATE
@@ -61,9 +75,48 @@ class UpdateClientState(Message):
 	def setLinesChanges(self, lines_changed):
 		self.lines_changed = lines_changed
 
+class DisconnectClient(Message):
+	type = MessageType.DISCONNECT_CLIENT
+		
+class KickClient(Message):
+	type = MessageType.KICK_CLIENT
 
+class CreateRoom(Message):
+	type = MessageType.CREATE_ROOM
+	length = 16
+
+	def setNumPlayers(self, num):
+		self.numPlayers = num
+	
+	def setNameLen(self, length):
+		self.nameLength = length
+
+	def setName(self, name):
+		self.name = name
+
+class UserAction(Message):
+	type = MessageType.USER_ACTION
+	length = 8
+
+	def setCmd(self, cmd):
+		self.cmd = cmd
 def main():
+	argv = sys.argv
+	
+	if len(argv) != 3:
+		print "Usage: ./test.py hostname port"
+		exit(0)
 
-	print("Hello world")
+	# Pop the head and ignore
+	argv = argv[1:]
 
+	hostname, port = argv[0], argv[1]
+
+	sock = socket.socket(socket.AF_INET,
+	                     socket.SOCK_DGRAM)
+
+	message = RegisterTetrad()
+
+	message.setName("I am a test client")	
+	sock.sento(message.pack(), (hostname, port))
 main()
